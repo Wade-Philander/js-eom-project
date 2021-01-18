1131cac9035af5e1e05c5da20d30094b9ae537b3
Wade-Philander <56066240+Wade-Philander@users.noreply.github.com>
2021-01-18 03:01:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,57 @@ $(document).ready(function(){
         }else{
             $(".navbar").removeClass("sticky");
         }
+        if(this.scroll > 500){
+            $(".scroll-up-btn").addClass("show");
+        }else{
+            $(".scroll-up-btn").removeClass("show");
+        }
     })
+
     $(".menu-btn").click(function(){
         $(".navbar .menu").toggleClass("active");
         $(".menu-btn i").toggleClass("active");
     });
+
+    //typing animation 
+    var typed = new Typed(".typing",{
+        strings:["The Future","a Developer","a Designer","a Content Creater"],
+        typeSpeed:100,
+        backspeed:60,
+        loop:true 
+    });
+    var typed = new Typed(".typing2",{
+        strings:["The Future","a Developer","a Designer","a Content Creater"],
+        typeSpeed:100,
+        backspeed:60,
+        loop:true 
+    });
+
+    // owl carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                item: 1,
+                nav: false
+            },
+            600:{
+                item: 2,
+                nav: false
+            },
+            1000:{
+                item: 3,
+                nav: false
+            }
+        }
+    });
 });
 
-const spans = document.querySelectorAll('text1')
+
+const spans = document.querySelectorAll('logo, span')
 spans.forEach(span => span.addEventListener('mouseover',function(e){
     span.classList.add('animated','rubberBand')
 }))
